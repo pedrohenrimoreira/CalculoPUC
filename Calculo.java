8d060b4d8cd470e64650ef05d58623ba76200562
Pedro Henrique Moreira <101280499+pedrohenrimoreira@users.noreply.github.com>
2022-12-01 06:32:48 -0300

    Update Calculo.java

--- Calculo.java
+++ Calculo.java
@@ -2,123 +2,123 @@
 //trabalho de calculo sobre consumo de agua em Java.
 
 public class Calculo{
-  static double faixaF(double vet[]) // Faixa 1 detalha o consumo para zero metros cúbicos
+  static double faixaF(double arr[]) 
 
   {
     double fixaAgua = 17.61; // taxa fixa de água
     double fixaEsgoto = 13.03; // taxa fixa de esgoto
     double fixaTotal = fixaAgua + fixaEsgoto;
-    System.out.println(" TAXA FIXA:            de esgoto R$"+ fixaEsgoto +"            de águ10a R$"+ fixaAgua);
+    System.out.println(" TAXA FIXA:            de esgoto R$"+ fixaEsgoto +"            de água R$"+ fixaAgua);
     System.out.println("Faixa de consumo:     Gasto em m³         Valor ");
-    vet[0] = fixaTotal;
+    arr[0] = fixaTotal;
     return 0;
   }
   
-  static double faixa2(double consumo, double vet[])
+  static double faixa2(double consumo, double arr[])
   {
     if (consumo > 0) {
-      faixaF(vet);
-      vet[1] = (1.820 + 1.350) * consumo;
-      System.out.println("Faixa entre 0m³ e 5m³      " + consumo + "      R$" + vet[1]);
+      faixaF(arr);
+      arr[1] = (1.820 + 1.350) * consumo;
+      System.out.println("Faixa entre 0m³ e 5m³      " + consumo + "      R$" + arr[1]);
       return 0;
     } else {
-      faixaF(vet);
-      vet[1] = (1.820 + 1.350) * consumo;
-      System.out.println("Faixa entre 0m³ e 5m³      " + consumo + "      R$" + vet[1]);
+      faixaF(arr);
+      arr[1] = (1.820 + 1.350) * consumo;
+      System.out.println("Faixa entre 0m³ e 5m³      " + consumo + "      R$" + arr[1]);
       return 0;
     }
   }
   
-  static double faixa3(double consumo, double vet[]) {
+  static double faixa3(double consumo, double arr[]) {
     double cfaixa = consumo - 5;
     if (consumo > 5) {
-      faixa2(5, vet);
-      vet[2] = (3.886 + 2.876) * cfaixa;
-      System.out.println("Faixa entre 5m³ e 10m³     " + cfaixa + "      R$" + vet[2]);
+      faixa2(5, arr);
+      arr[2] = (3.886 + 2.876) * cfaixa;
+      System.out.println("Faixa entre 5m³ e 10m³     " + cfaixa + "      R$" + arr[2]);
       return 0;
     } else {
-      faixa2(consumo, vet);
+      faixa2(consumo, arr);
       cfaixa = 0;
-      vet[2] = (3.886 + 2.876) * cfaixa;
-      System.out.println("Faixa entre 5m³ e 10m³     " + cfaixa + "      R$" + vet[2]);
+      arr[2] = (3.886 + 2.876) * cfaixa;
+      System.out.println("Faixa entre 5m³ e 10m³     " + cfaixa + "      R$" + arr[2]);
       return 0;
     }
   }
   
-  static double faixa4(double consumo, double vet[]) {
+  static double faixa4(double consumo, double arr[]) {
     double cfaixa = consumo - 10;
     if (consumo > 10) {
-      faixa3(10, vet);
-      vet[3] = (6.023 + 4.457) * cfaixa;
-      System.out.println("Faixa entre 10m³ e 15m³    " + cfaixa + "      R$" + vet[3]);
+      faixa3(10, arr);
+      arr[3] = (6.023 + 4.457) * cfaixa;
+      System.out.println("Faixa entre 10m³ e 15m³    " + cfaixa + "      R$" + arr[3]);
       return 0;
     } else {
-      faixa3(consumo, vet);
+      faixa3(consumo, arr);
       cfaixa = 0;
-      vet[3] = (6.023 + 4.457) * cfaixa;
-      System.out.println("Faixa entre 10m³ e 15m³     " + cfaixa + "     R$" + vet[3]);
+      arr[3] = (6.023 + 4.457) * cfaixa;
+      System.out.println("Faixa entre 10m³ e 15m³     " + cfaixa + "     R$" + arr[3]);
       return 0;
     }
   }
   
-  static double faixa5(double consumo, double vet[]) {
+  static double faixa5(double consumo, double arr[]) {
     double cfaixa = consumo - 15;
     if (consumo > 15) {
-      faixa4(15, vet);
-      vet[4] = (8.222 + 6.084) * cfaixa;
-      System.out.println("Faixa entre 15m³ e 20m³    " + cfaixa + "      R$" + vet[4]);
+      faixa4(15, arr);
+      arr[4] = (8.222 + 6.084) * cfaixa;
+      System.out.println("Faixa entre 15m³ e 20m³    " + cfaixa + "      R$" + arr[4]);
       return 0;
     } else {
-      faixa4(consumo, vet);
+      faixa4(consumo, arr);
       cfaixa = 0;
-      vet[4] = (8.222 + 6.084) * cfaixa;
-      System.out.println("Faixa entre 15m³ e 20m³    " + cfaixa + "      R$" + vet[4]);
+      arr[4] = (8.222 + 6.084) * cfaixa;
+      System.out.println("Faixa entre 15m³ e 20m³    " + cfaixa + "      R$" + arr[4]);
       return 0;
     }
   }
   
-  static double faixa6(double consumo, double vet[]) {
+  static double faixa6(double consumo, double arr[]) {
     double cfaixa = consumo - 20;
     if (consumo > 20) {
-      faixa5(20, vet);
-      vet[5] = (10.458 + 7.739) * cfaixa;
-      System.out.println("Faixa entre 20m³ e 40m³    " + cfaixa + "     R$" + vet[5]);
+      faixa5(20, arr);
+      arr[5] = (10.458 + 7.739) * cfaixa;
+      System.out.println("Faixa entre 20m³ e 40m³    " + cfaixa + "     R$" + arr[5]);
       return 0;
     } else {
-      faixa5(consumo, vet);
+      faixa5(consumo, arr);
       cfaixa = 0;
-      vet[5] = (10.458 + 7.739) * cfaixa;
-      System.out.println("Faixa entre 20m³ e 40m³     " + cfaixa + "      R$" + vet[5]);
+      arr[5] = (10.458 + 7.739) * cfaixa;
+      System.out.println("Faixa entre 20m³ e 40m³     " + cfaixa + "      R$" + arr[5]);
       return 0;
     }
   }
   
-  static double faixa7(double consumo, double vet[])
+  static double faixa7(double consumo, double arr[])
   {
     double cfaixa = consumo - 40;
     if (consumo > 40) {
-      faixa6(40, vet);
-      vet[6] = (12.759 + 9.441) * cfaixa;
-      System.out.println("Faixa acima de 40m³      " + cfaixa + "     R$" + vet[6]);
+      faixa6(40, arr);
+      arr[6] = (12.759 + 9.441) * cfaixa;
+      System.out.println("Faixa acima de 40m³      " + cfaixa + "     R$" + arr[6]);
       return 0;
     } else {
-      faixa6(consumo, vet);
+      faixa6(consumo, arr);
       cfaixa = 0;
-      vet[6] = (12.759 + 9.441) * cfaixa;
-      System.out.println("Faixa acima de 40m³      " + cfaixa + "     R$" + vet[6]);
+      arr[6] = (12.759 + 9.441) * cfaixa;
+      System.out.println("Faixa acima de 40m³      " + cfaixa + "     R$" + arr[6]);
       return 0;
     }
   }
-  static double valorDaConta(double vet[]) 
+  static double valorDaConta(double arr[]) 
   {
     double soma = 0;
     for (int c = 0; c < 7; c++) {
-      soma += vet[c];
+      soma += arr[c];
     }
     return soma;
   }
   public static void main(String[] args) {
-    double vet[] = new double[7];//declara o vetor que armazenará os valores em cada faixa
+    double arr[] = new double[7];   
     double consumo;//declara o consumo em metros cúbicos
     double valor;
     System.out.println("Informe o consumo em m³:");
@@ -127,11 +127,11 @@ public class Calculo{
     if (consumo < 0) // caso seja informado consumo abaixo de zero,retornar erro
                     
     {
-      System.out.println("valor informado incorretamente");
+      System.out.println("ERRO, valor incorreto");
     } else {
-      faixa7(consumo, vet);
+      faixa7(consumo, arr);
     }
-    valor = valorDaConta(vet);
-    System.out.println("Valor a pagar nesta fatura: R$" + valor);
+    valor = valorDaConta(arr);
+    System.out.println("Valor a ser pago: R$" + valor);
   }
 }
